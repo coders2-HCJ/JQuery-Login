7d42db60fb942b4e3a84792c67759c8ccd66db4a
kitteneditorc0der <115348427+kitteneditorc0der@users.noreply.github.com>
2022-10-14 08:44:49 +0800

    Create interactive.js

---
+++ interactive.js
@@ -0,0 +1,21 @@
+// Must read ↓
+var account = {
+    username: "khan_academy",
+    password: "ka2008"
+};
+$(document).ready(function() {
+    $("form").on("submit", function(event) {
+        event.preventDefault();
+        if($("#username").val() === account.username && $("#password").val() === account.password) {
+            $("body")
+                .text("Hooray!")
+                .css("font-size", "100px")
+                .css("border", "10px dotted orange")
+                .css("margin-top", "150px")
+                .hide()
+                .delay(500)
+                .slideDown(1000)
+            ;
+        }
+    });
+});
